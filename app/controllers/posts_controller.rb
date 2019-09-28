8cdd5624e7248fd7745a8d19c13b0656f7d44dca
IiyamaMakoto <m.iiyama@gmail.com>
2019-09-29 00:07:59 +0900

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.order(id: 'desc')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text).merge(user_id: current_user[:id])
+  end
+end
