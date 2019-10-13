408fa286d5b2d22303cc679f860906ede497effe
IiyamaMakoto <m.iiyama@gmail.com>
2019-10-13 13:28:07 +0900

    edit user_controller
    記事を投稿していないユーザーの記事一覧を表示した時に、記事がない旨を表示させるように変更。
    （@postsが空の時にメッセージがない旨のメッセージpostを生成、表示）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   def show
     @posts = Post.includes(:user).where(user_id: params[:id]).order(id: 'desc')
     @users = User.order(id: 'asc')
+    if @posts == []
+      @posts = Post.new(title: "投稿なし", text: "このユーザーの投稿はまだありません")
+    end
   end
 
 end
